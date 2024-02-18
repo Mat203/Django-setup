05ec0752c4071eb9321e05d638e37fe6edacebe2
Mat_Tol <matvey.talalaevskiy@gmail.com>
2024-02-18 20:39:26 +0200

    final version

--- hw9_talalaievskyi/playground/static/scripts/chat.js
+++ hw9_talalaievskyi/playground/static/scripts/chat.js
@@ -1,8 +1,24 @@
+let nickname = prompt("Please enter your nickname:");
+
 const chatSocket = new WebSocket("ws://127.0.0.1:8000/ws");
 
 const messagesContainer = document.getElementById('messages');
 const nameInput = document.getElementById('name');
 const messageInput = document.getElementById('message');
+
+nameInput.value = nickname;
+
+const cachedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+const cleanHistoryButton = document.getElementById('clean-history');
+cachedMessages.forEach(msg => writeToScreen(msg.user, msg.text, msg.type));
+
+messageInput.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault(); 
+        sendMessage();
+    }
+});
+
 
 function sendMessage() {
     const name = nameInput.value;
@@ -20,12 +36,17 @@ function sendMessage() {
     messageInput.value = ''
 }
 
-function writeToScreen(user, message, type) {
+function writeToScreen(user, message) {
     const p = document.createElement('p');
     p.style.margin = '20px';
-    p.className = type;
+    p.className = user === nickname ? 'sent' : 'received';
     p.innerHTML = `<strong>${user}:</strong> ${message}`;
     messagesContainer.appendChild(p);
+
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+    cachedMessages.push({user, text: message, type: p.className});
+    localStorage.setItem('chatMessages', JSON.stringify(cachedMessages));
 }
 
 chatSocket.onmessage = function(e) {
@@ -37,3 +58,11 @@ chatSocket.onmessage = function(e) {
     const type = beMessageJSON.user === nameInput.value ? 'sent' : 'received';
     writeToScreen(beMessageJSON.user, beMessageJSON.text, type)
 };
+
+cleanHistoryButton.addEventListener('click', function() {
+    while (messagesContainer.firstChild) {
+        messagesContainer.removeChild(messagesContainer.firstChild);
+    }
+
+    localStorage.removeItem('chatMessages');
+});
